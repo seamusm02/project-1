c22c4bfff1a9d12714c558ebd1b9ccd6f90ae513
Seamus Murphy <seamusm02@gmail.com>
2020-02-17 05:10:15 -0600

    Updated include function to append scripts to bottom of body

--- js/main.js
+++ js/main.js
@@ -35,9 +35,7 @@ function include(file) {
   
     let script  = document.createElement('script'); 
     script.src  = file; 
-    script.type = 'text/javascript'; 
-    script.defer = false; 
+    script.defer = true; 
         
-    document.getElementsByTagName('head').item(0).appendChild(script); 
-  
+    document.querySelector("body").appendChild(script);
 }
